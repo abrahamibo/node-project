53b9156bdc51125e9b09d1f35f8f5820bd5985cd
abrahamibo <abraham.ibo@hetic.net>
2020-03-13 18:11:59 +0100

    start dev test nodeJS

--- index.js
+++ index.js
@@ -1,7 +1,68 @@
-var http = require('http');
+let http = require('http');
+let url = require('url');
+var parseFormdata = require('parse-formdata')
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end('Salut tout le monde !');
+
+let querystring = require('querystring');
+let EventEmitter = require('events').EventEmitter;
+
+let jeu = new EventEmitter();
+
+jeu.on('gamestart', function(message){
+    console.log(message);
 });
-server.listen(3001);
+let server = http.createServer((req, res) => {
+    let page = url.parse(req.url).pathname;
+    let params = querystring.parse(url.parse(req.url).query);
+
+    console.log(page);
+    console.log(params);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    switch (page) {
+        case '/':
+            res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
+            break;
+        case '/sous-sol':
+            res.write('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+            break;
+        case '/game2':
+            parseFormdata(req, function (err, data) {
+                if (err) {
+                    console.log(err);
+                    // console.log("err");
+                    // throw err
+                }
+                console.log('fields:', data)
+                // data.parts.forEach(function (part) {
+                //     console.log('part:', part)
+                // })
+                // res.end('Be kind to each other')
+            })
+            break;
+        case '/game':
+            let queryData;
+            let data = []
+            req.on('data', chunk => {
+                data.push(chunk)
+            })
+            req.on('end', () => {
+
+                const result = Buffer.concat(data).toString();
+                console.log(result);
+                // console.log(JSON.parse(data)); // 'Buy the milk'
+            })
+
+
+            // jeu.emit('gamestart', data);
+            break;
+
+    }
+    res.end();
+});
+
+
+
+
+server.listen(3001,() =>{
+    console.log('start')
+});
